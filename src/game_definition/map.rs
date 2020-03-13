f2c2743c1b0eda93c7834a6d1d9e5d64eada38dd
Paul Lesur <git@lesurpaul.fr>
2020-03-13 16:45:27 +0100

    add tests for id <=> xy conversions

--- src/game_definition/map.rs
+++ src/game_definition/map.rs
@@ -28,8 +28,25 @@ pub struct GameMap {
 }
 
 impl GameMap {
+    pub fn new(width: usize, data: Vec<Cell>) -> GameMap {
+        let height = data.len() / width;
+        assert!(width * height == data.len());
+
+        GameMap {
+            width,
+            height,
+            data,
+        }
+    }
+
     pub fn xy_to_id(&self, x: usize, y: usize) -> CellId {
         CellId::new(y * self.width + x)
+    }
+
+    pub fn id_to_xy(&self, id: CellId) -> (usize, usize) {
+        let x = id.raw() % self.width;
+        let y = (id.raw() - x) / self.width;
+        (x, y)
     }
 }
 
@@ -38,5 +55,62 @@ impl Index<CellId> for GameMap {
 
     fn index(&self, id: CellId) -> &Self::Output {
         &self.data[id.raw()]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::CellId;
+    use super::GameMap;
+
+    #[test]
+    fn test_xy_to_id() {
+        let w = 4;
+        let h = 6;
+        let map = GameMap {
+            width: w,
+            height: h,
+            data: Vec::new(),
+        };
+
+        let x = 1;
+        let y = 3;
+
+        let cell_id = map.xy_to_id(x, y);
+
+        assert_eq!(cell_id.raw(), 13);
+    }
+
+    #[test]
+    fn test_id_to_xy() {
+        let w = 4;
+        let h = 6;
+        let map = GameMap {
+            width: w,
+            height: h,
+            data: Vec::new(),
+        };
+
+        let cell_id = CellId::new(22);
+        let (x, y) = map.id_to_xy(cell_id);
+
+        assert_eq!(x, 2);
+        assert_eq!(y, 5);
+    }
+
+    #[test]
+    fn test_id_consistency() {
+        let w = 4;
+        let h = 6;
+        let map = GameMap {
+            width: w,
+            height: h,
+            data: Vec::new(),
+        };
+
+        let cell_id = CellId::new(22);
+        let (x, y) = map.id_to_xy(cell_id);
+
+        assert_eq!(cell_id, map.xy_to_id(x, y));
     }
 }
