c708d17ceb31adb0e80625a4e1ac2f5443ab11e3
Paul Lesur <git@lesurpaul.fr>
2020-03-20 18:08:31 +0100

    derive debug

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,10 +1,10 @@
 use crate::game::{Action, Cell, Class, GameMap, Id, Team};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WireAction(pub Action);
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WireNewCharRequest {
     pub game: String,
     pub name: String,
@@ -13,13 +13,13 @@ pub struct WireNewCharRequest {
     pub position: Id<Cell>,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WireCreatedGame(pub String);
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WireCreatedChar(pub String);
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WireNewGameRequest {
     pub map: Id<GameMap>,
     pub team_size: usize,
